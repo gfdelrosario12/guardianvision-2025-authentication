84d6f851aa70a955c66615e34e86990ac01792fc
Gladwin Ferdz Del Rosario <gladelrosario12@gmail.com>
2025-07-14 12:23:56 +0800

    fix: modified cors to cater vercel

--- src/main/java/com/guardianvision/backend/config/WebConfig.java
+++ src/main/java/com/guardianvision/backend/config/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns(
-                                "https://*.vercel.app",
+                        .allowedOrigins(
+                                "https://guardian-vision.vercel.app", // <- exact match
                                 "http://localhost:5173",
                                 "http://localhost:3000"
                         )
